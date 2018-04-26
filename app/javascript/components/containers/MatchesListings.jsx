5e77bdc578104752bdffb77877aadc1108401e59
Banele <gbmkholo@yahoo.co.za>
2018-04-26 09:34:23 +0200

    add functionality to the next button

--- app/javascript/components/containers/MatchesListings.jsx
+++ app/javascript/components/containers/MatchesListings.jsx
@@ -15,15 +15,63 @@ import Veronika from 'images/veronika.jpg'
 class MatchesListings extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = { arrows: "hidden", ...props }
+		this.state = {
+			images: [
+				{ name: "Ade", image: Ade, selectionClass: "selectedImage " },
+				{ name: "Daniel", image: Daniel, selectionClass: "" },
+				{ name: "Veronika", image: Veronika, selectionClass: "" },
+				{ name: "Jenny", image: Jenny, selectionClass: "" }
+			],
+			indexForActiveImage: 0,
+			arrows: {
+				left: "hidden",
+				right: "hidden"
+			}, 
+			...props
+		}
 	}
 
 	showArrows = () => {
-		this.setState({arrows: "visible"})
+		let updatedArrows = this.state.arrows
+		if(this.state.indexForActiveImage > 0) {
+			updatedArrows = { ...updatedArrows, left: "visible"}
+		}
+		if(this.state.indexForActiveImage < 3) {
+			updatedArrows = { ...updatedArrows, right: "visible"}
+		}
+		this.setState({arrows: updatedArrows})
 	}
 
 	hideArrows = () => {
-		this.setState({arrows: "hidden"})
+		this.setState({
+			arrows: {
+				left: "hidden",
+				right: "hidden"
+			}
+		})
+	}
+
+	shiftIndexToTheRight = () => {
+		if(this.state.indexForActiveImage >= this.state.images.length-1) {
+			return
+		} else {
+			let images = this.state.images
+			let newIndex = this.state.indexForActiveImage + 1
+			images[this.state.indexForActiveImage] = {...this.state.images[this.state.indexForActiveImage], selectionClass: ""}
+			images[this.state.indexForActiveImage+1] = {...this.state.images[this.state.indexForActiveImage+1], selectionClass: "selectedImage "}
+			this.setState({images: images, indexForActiveImage: newIndex})
+		}
+	}
+
+	shiftIndexToTheLeft = () => {
+		if(this.state.indexForActiveImage <= 0) {
+			return
+		} else {
+			let images = this.state.images
+			images[this.state.indexForActiveImage] = {...this.state.images[this.state.indexForActiveImage], selectionClass: ""}
+			images[this.state.indexForActiveImage-1] = {...this.state.images[this.state.indexForActiveImage-1], selectionClass: "selectedImage "}
+			this.setState({images: images, indexForActiveImage: this.state.indexForActiveImage - 1})
+		}
 	}
 
 	render() {
@@ -33,15 +81,21 @@ class MatchesListings extends React.Component {
           { show && (<div className="ui list cards">
             <div className="ui raised card three wide column">
               <div className="ui image below" onMouseOver={this.showArrows} onMouseLeave={this.hideArrows}>
-              	<i className="angle huge left icon onTop toTheLeft" style={{visibility: this.state.arrows}}></i>
-                <i className="angle huge right icon onTop toTheRight" style={{visibility: this.state.arrows}}></i>
-                <img src={Ade} className="visible content" />
+              	<button class="circular ui icon primary basic button onTop toTheLeft" onClick={this.shiftIndexToTheLeft} style={{visibility: this.state.arrows.left, cursor: "pointer"}}>
+              	 <i className="angle large left icon"></i>
+                </button>
+                <button className="circular ui primary basic icon button onTop toTheRight" onClick={this.shiftIndexToTheRight} style={{visibility: this.state.arrows.right, cursor: "pointer"}}>
+                	<i className="angle large right icon"></i>
+                </button>
+                <img src={this.state.images[this.state.indexForActiveImage]["image"]} className="visible content" />
               </div>
               
-              <div className="ui tiny images" style={{paddingTop: "5px"}}>
-              	<img src={Daniel} className="ui image rounded"/>
-              	<img src={Jenny} className="ui image rounded"/>
-              	<img src={Veronika} className="ui image rounded"/>
+              <div className="ui mini images" style={{paddingTop: "5px"}}>
+              	{
+              		this.state.images.map((imageHash) =>
+              			<img key={imageHash.name} src={imageHash.image} className={imageHash.selectionClass + "ui image rounded"} />
+              		)
+              	}
               </div>
 
               <div className="content">
