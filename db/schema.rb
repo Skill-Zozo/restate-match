9236dfd6593026cc6e0e3fc1df23dae27407f01d
Banele <gbmkholo@yahoo.co.za>
2018-05-16 23:30:48 +0200

    use active storage to serve images

--- db/schema.rb
+++ db/schema.rb
@@ -10,59 +10,80 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402183350) do
+ActiveRecord::Schema.define(version: 2018_05_14_224307) do
 
   create_table "accomodation_requests", force: :cascade do |t|
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.integer  "user_id"
-    t.boolean  "furnished"
-    t.string   "near"
-    t.integer  "max_price"
-    t.integer  "min_price"
-    t.integer  "min_bedroom"
-    t.integer  "max_bedroom"
-    t.boolean  "internet_access"
-    t.string   "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.boolean "furnished"
+    t.string "near"
+    t.integer "max_price"
+    t.integer "min_price"
+    t.integer "min_bedroom"
+    t.integer "max_bedroom"
+    t.boolean "internet_access"
+    t.string "location"
     t.index ["user_id"], name: "index_accomodation_requests_on_user_id"
   end
 
   create_table "accomodations", force: :cascade do |t|
     t.integer "user_id"
-    t.string  "street_address"
-    t.string  "suburb"
-    t.string  "city"
-    t.string  "province"
-    t.string  "country"
+    t.string "street_address"
+    t.string "suburb"
+    t.string "city"
+    t.string "province"
+    t.string "country"
     t.integer "bedroom_count"
     t.integer "price"
     t.integer "bathroom_count"
-    t.string  "lounge_description"
-    t.string  "bathroom_description"
-    t.string  "bedroom_description"
+    t.string "lounge_description"
+    t.string "bathroom_description"
+    t.string "bedroom_description"
     t.integer "garage_count"
-    t.string  "garage_description"
-    t.string  "kitchen_description"
+    t.string "garage_description"
+    t.string "kitchen_description"
     t.boolean "furnished"
-    t.string  "internet_access"
-    t.string  "near"
+    t.string "internet_access"
+    t.string "near"
     t.index ["user_id"], name: "index_accomodations_on_user_id"
   end
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "type"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
