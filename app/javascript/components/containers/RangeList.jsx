49112f45fda4441b77cc12f2324513bb3731ada4
Banele <gbmkholo@yahoo.co.za>
2018-05-28 21:58:29 +0200

    neater form, one way binding of data btwn range and numeric input

--- app/javascript/components/containers/RangeList.jsx
+++ app/javascript/components/containers/RangeList.jsx
@@ -29,11 +29,7 @@ class RangeList extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-<<<<<<< HEAD
 			...this.props
-=======
-			...this.props.rangeItems
->>>>>>> 7dd14008f431a9cf283942cf925b350d8c251f0b
 		}
 	}
 
@@ -48,60 +44,45 @@ class RangeList extends Component {
 	}
 
   setRangeMarks = (values) => {
-    let activeValues = this.state[rangeItemName]
-    activeValues[rangeItemName].min = values[0]
-    activeValues[rangeItemName].max = values[1]
+    let activeValues = this.state[this]
+    activeValues[this].min = values[0]
+    activeValues[this].max = values[1]
     this.setState({rangeItemName: activeValues})
     rangeItem['setItemValue'](values)
   }
 
   setMin = (event) => {
-    let activeValues = this.state[event.name]
-    activeValues[event.name].min = event.val
-    let values = [event.val, this.state[event.name].max]
-    let changedObject = {}
-    changedObject[event.name] = activeValues
-    this.setState(changedObject)
-    this.state[event.name]['setItemValue'](values)
+    let values = [event.target.value, this.state.rangeItems[event.target.name].max]
+    this.state.rangeItems[event.target.name]['setItemValue'](values)
   }
 
   setMax = (event) => {
-    let activeValues = this.state[event.name]
-    activeValues[event.name].max = event.val
-    let values = [this.state[event.name].min, event.val]
-    let changedObject = {}
-    changedObject[event.name] = activeValues
-    this.setState(changedObject)
-    this.state[event.name]['setItemValue'](values)
+    let values = [this.state.rangeItems[event.target.name].min, event.target.value]
+    this.state.rangeItems[event.target.name]['setItemValue'](values)
   }
 
 	render () {
 		let itemsKeys =  _.keys(this.state.rangeItems)
-    // console.log(underscore)
-    // if(['price'] != undefined) {
-    //   debugger
-    //   itemsKeys = underscore.keys(this.state.rangeItems)
-    // }
-    // console.log(itemsKeys)
-    // console.log(this.state.rangeItems)
 		return (
-      <div className='ui grid'>
+      <div className='ui form' style={{padding: '15px'}}>
         { 
           itemsKeys.map((rangeItemName, i) => (
-            <div className='item row spaced' key={rangeItemName}>
-              <div className='ui grid' style={{margin: '12px', width:'100%'}}>
-                <div className="ui two column wide horizontal left-borded-label basic label large" style={{padding: '12px 2px'}}> {rangeItemName}</div>
-                <div className="middle eight wide column aligned content" style={{margin: '0px 10px 0px 10px'}}>
+            <div className='field' key={rangeItemName}>
+              <h4 className="ui header">{rangeItemName}</h4>
+              <div className="fields">
+                <div className='eleven wide field'>
                   <Range className='column' min={this.state.rangeItems[rangeItemName].min} max={this.state.rangeItems[rangeItemName].max} 
                           handle={handles} marks={this.marks(this.state.rangeItems[rangeItemName].min, this.state.rangeItems[rangeItemName].max)} style={{fontSize: '150%'}}
-                          onChange={this.setRangeMarks.bind(rangeItemName)} defaultValue={[this.state.rangeItems[rangeItemName].min, this.state.rangeItems[rangeItemName].max]}/>
+                          onChange={this.state.rangeItems[rangeItemName]['setItemValue']} defaultValue={[this.state.rangeItems[rangeItemName].min, this.state.rangeItems[rangeItemName].max]}/>
                 </div>
-                <div className="ui three wide column input focus mini">
-                  <input type="number" placeholder={`${this.state.rangeItems[rangeItemName].min}`} name={rangeItemName} value={this.state.rangeItems[rangeItemName].min} onChange={this.setMin}/>
+                <div className='two wide field'>
+                  <input type="number" placeholder={`${this.state.rangeItems[rangeItemName].min}`} name={rangeItemName} value={this.props.rangeItems[rangeItemName].min} onChange={this.setMin}/>
                 </div>
-                <div style={{padding: '15px 0px', margin: '0'}}>-</div>
-                <div className="ui three wide column input focus mini">
-                  <input type="number" placeholder={`${this.state.rangeItems[rangeItemName].max}`} name={rangeItemName} value={this.state.rangeItems[rangeItemName].max} onChange={this.setMax}/>
+                <div className='one wide field' style={{textAlign: 'center'}}>
+                  -
+                </div>
+                <div className='two wide field'>
+                  <input type="number" placeholder={`${this.state.rangeItems[rangeItemName].max}`} name={rangeItemName} value={this.props.rangeItems[rangeItemName].max} onChange={this.setMax}/>
                 </div>
               </div>
             </div>
